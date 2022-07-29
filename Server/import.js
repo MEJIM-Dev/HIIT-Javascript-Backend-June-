f4cfc2fa7e32bb4d297ba0a81fbd6ba31771da0c
taskmastarh <taskmastarh@gmail.com>
2022-07-29 13:11:08 +0100

    Working with modules

---
+++ Server/import.js
@@ -0,0 +1,18 @@
+const exportVar = "ertyukjljhgfdsjhgrfdsadfhjgfdsfghjkljhgfdsfghjkhgfdsfghjkhgfdsfghjkhgfdghjkhgfdghjkhgfdghjkhg"
+const mYfunction = () => {
+    console.log("imported function")
+}
+
+function dfghjkl () {
+
+} 
+
+function hjkl() {
+
+}
+
+module.exports = {
+    exportVar: exportVar,
+    mYfunction: mYfunction,
+    dfghjkl: dfghjkl
+};
---
+++ Server/server.js
@@ -0,0 +1,23 @@
+console.log("node at work")
+
+const fs = require("fs")
+
+fs.writeFileSync("./data.txt","sdfghjkljhgfd")
+
+const {myFunction} = require("./import")
+
+const importFunction= require("./import")
+
+// const myFunction = importFunction.mYfunction;
+
+// importFunction.dfghjkl
+// importFunction.exportVar;
+// console.log(importFunction)
+// importFunction.mYfunction()
+// myFunction()
+
+// const obj = {
+//     name: "sdfghjkl;'",
+//     title: "fdghjkl;",
+//     page: "4567890"
+// }
