d8b7042cca27f2aa517882ee00ca125f52340c69
taskmastarh <taskmastarh@gmail.com>
2022-08-09 12:51:39 +0100

    Basic setup ofan exprees server & serving some static files to clients

---
+++ Server/basics/importScripts.js
@@ -0,0 +1,171 @@
+// const {myFunction, obj} = require("./import")
+// const personalModule = require("./import")
+// const fs = require("fs")
+
+// fs.writeFileSync("./new.txt","sedrgyhujkljhgfd")
+// fs.writeFile("./new.txt","sedrgyhujkljhgfd",(err)=>{
+//     if(err) {
+//         console.log("failed to write")
+//     } else{
+//         console.log("File written successfully")
+//     } 
+// })
+
+function skipOddNumbers(arg){
+    let answer = []
+
+    arg.forEach((data,index)=>{
+        if(index%2!==0){
+
+        } else{
+            answer.push(data)
+        } 
+    })
+    // for (let i = 0; i < arg.length; i++) {
+    //     const element = arg[i];
+    //     if(i%2!==0){
+
+    //     } else{
+    //         answer.push(element)
+    //     }        
+    // }
+    console.log(answer)
+    return answer
+}
+
+const tim = skipOddNumbers(["ab","ac","ad","sc", "hgthui"])
+// console.log(tim)
+// const fs = require("fs")
+
+// fs.writeFileSync("./sat.txt", "kjhyrdsa")
+// const data =fs.readFileSync("./new.json",{encoding:"utf-8"})
+// console.log(data)   
+// fs.appendFileSync("./sat.txt", "\n kjhyrdsa")
+
+// try {
+//     const dataFromSync = fs.readFileSync("./new.txt")
+
+//     console.log("File read successfully")
+//     console.log(dataFromSync.toString())
+// } catch (error) {
+//     console.log("failed to read")
+// }
+
+// fs.writeFile("./new.json",JSON.stringify(obj,null,2),(err)=>{
+//     if(err){
+//         console.log("failed to writ file")
+//     } else{
+//         console.log("successful")
+//     }
+// })
+
+// fs.writeFile("./new.js","function test() { console.log() } \n module.exports = test;",(err)=>{
+//     if(err){
+//         console.log("failed to writ file")
+//     } else{
+//         console.log("successful")
+//     }
+// })
+
+const rp = "./new.js"
+
+// function insertFunction(path,data){
+//     if(fs.existsSync(path)){
+//         const oldData = fs.readFileSync(path, {encoding:"utf-8"})
+        
+//         const oldObj = JSON.parse(oldData)
+
+//         oldObj.push(data)
+//         oldObj.forEach(e => {
+//             console.log(e)
+//         });
+        
+//         fs.writeFile(path, JSON.stringify(oldObj,null,2), (err)=>{
+//             if(err){
+//                 console.log("failed")
+//             } else{
+//                 console.log("File updated")
+//             }
+//         })
+        
+//     }else{
+
+//         fs.writeFile(path, JSON.stringify([data],null,2), (err)=>{
+//             if(err){
+//                 console.log("failed")
+//             } else{
+//                 console.log("File created")
+//             }
+//         })
+//     }
+// }
+
+// insertFunction("./a.json",obj)
+
+// if(!fs.existsSync("./new.js")){
+//     fs.writeFile("./new.js","function test() { console.log() } \n module.exports = test;",(err)=>{
+//         if(err){
+//             console.log("failed to writ file")
+//         } else{
+//             console.log("successful")
+//         }
+//     })
+// }else{
+//     fs.appendFile("./new.js","\n function test2() { console.log() } \n module.exports = test;",(err)=>{
+//         console.log("done")
+//     })
+// }
+
+
+
+// fs.readFile("./new.txt",{encoding:"utf-8"},(err,data)=>{
+//     if(err){
+//         console.log("failed to read")
+//     } else{
+//         console.log("File read successfully")
+//         console.log("data:", data)
+
+//         fs.writeFile("./second.txt", data, (err)=>{
+//             if(err){
+//                 console.log("failed to write to file")
+//             } else{
+//                 console.log("done")
+//             }
+//         })
+//     } 
+// })
+
+// const importFunction = require("./import")
+
+// const objRecieved = importFunction.obj;
+
+// console.log("node at work")
+
+
+
+
+
+
+
+
+
+
+
+
+// const myFunction = importFunction.mYfunction;
+
+//const fs = require("fs")
+
+// fs.writeFileSync("./data.txt","sdfghjkljhgfd")
+
+// importFunction.dfghjkl
+// importFunction.exportVar;
+// console.log(importFunction)
+// importFunction.mYfunction()
+// myFunction()
+
+// const obj = {
+//     name: "sdfghjkl;'",
+//     title: "fdghjkl;",
+//     page: "4567890"
+// }
--- Server/server.js
+++ Server/server.js
@@ -1,171 +1,110 @@
-// const {myFunction, obj} = require("./import")
-// const personalModule = require("./import")
-// const fs = require("fs")
+const express = require("express")
+const fs = require("fs")
+const path = require("path")
 
-// fs.writeFileSync("./new.txt","sedrgyhujkljhgfd")
-// fs.writeFile("./new.txt","sedrgyhujkljhgfd",(err)=>{
-//     if(err) {
-//         console.log("failed to write")
-//     } else{
-//         console.log("File written successfully")
-//     } 
+const app = express()
+
+app.listen(5000)
+
+const users = ["sam","ken","sarah"]
+
+const public = path.join(__dirname,"public")
+//Setting up exposed/public directory for access from clients
+app.use(express.static(public))
+
+app.get("/", (req,res)=>{
+    console.log(req.url)
+    const page = fs.readFileSync("./public/index.html",{encoding:"utf-8"})
+    res.send("index")
+})
+
+app.get("/about", (req,res)=>{
+    console.log(req.url)
+    const page = fs.readFileSync("./public/about.html",{encoding:"utf-8"})
+    res.sendFile(path.join(__dirname,"public","about.html"))
+})
+
+app.get("/contact",(req,res)=>{
+    console.log(req.url)
+    res.send("welcome to the contact page")
+})
+
+app.post("/register",(req,res)=>{
+    const possibleUser = req.query.username
+    
+    console.log(req.url)
+
+    if(users.includes(possibleUser)){
+        return res.end("User already exist")
+    } else{
+        users.push(possibleUser)
+        return res.send("user is been registered")
+    }
+    
+})
+
+app.get("/*", (req,res)=>{
+    res.status(404).send("Resource not found")
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// app.post("/api/register",(req,res)=>{
+//     console.log(req.query)
+//     res.status(200).end()
 // })
 
-function skipOddNumbers(arg){
-    let answer = []
+// const http = require("http")
 
-    arg.forEach((data,index)=>{
-        if(index%2!==0){
+// http.createServer(function(req,res){
 
-        } else{
-            answer.push(data)
-        } 
-    })
-    // for (let i = 0; i < arg.length; i++) {
-    //     const element = arg[i];
-    //     if(i%2!==0){
+//     console.log(req.url)
 
-    //     } else{
-    //         answer.push(element)
-    //     }        
-    // }
-    console.log(answer)
-    return answer
-}
-
-const tim = skipOddNumbers(["ab","ac","ad","sc", "hgthui"])
-// console.log(tim)
-// const fs = require("fs")
-
-// fs.writeFileSync("./sat.txt", "kjhyrdsa")
-// const data =fs.readFileSync("./new.json",{encoding:"utf-8"})
-// console.log(data)   
-// fs.appendFileSync("./sat.txt", "\n kjhyrdsa")
-
-// try {
-//     const dataFromSync = fs.readFileSync("./new.txt")
-
-//     console.log("File read successfully")
-//     console.log(dataFromSync.toString())
-// } catch (error) {
-//     console.log("failed to read")
-// }
-
-// fs.writeFile("./new.json",JSON.stringify(obj,null,2),(err)=>{
-//     if(err){
-//         console.log("failed to writ file")
-//     } else{
-//         console.log("successful")
-//     }
-// })
-
-// fs.writeFile("./new.js","function test() { console.log() } \n module.exports = test;",(err)=>{
-//     if(err){
-//         console.log("failed to writ file")
-//     } else{
-//         console.log("successful")
-//     }
-// })
-
-const rp = "./new.js"
-
-// function insertFunction(path,data){
-//     if(fs.existsSync(path)){
-//         const oldData = fs.readFileSync(path, {encoding:"utf-8"})
-        
-//         const oldObj = JSON.parse(oldData)
-
-//         oldObj.push(data)
-//         oldObj.forEach(e => {
-//             console.log(e)
-//         });
-        
-//         fs.writeFile(path, JSON.stringify(oldObj,null,2), (err)=>{
-//             if(err){
-//                 console.log("failed")
-//             } else{
-//                 console.log("File updated")
-//             }
-//         })
-        
-//     }else{
-
-//         fs.writeFile(path, JSON.stringify([data],null,2), (err)=>{
-//             if(err){
-//                 console.log("failed")
-//             } else{
-//                 console.log("File created")
-//             }
-//         })
-//     }
-// }
-
-// insertFunction("./a.json",obj)
-
-// if(!fs.existsSync("./new.js")){
-//     fs.writeFile("./new.js","function test() { console.log() } \n module.exports = test;",(err)=>{
-//         if(err){
-//             console.log("failed to writ file")
-//         } else{
-//             console.log("successful")
-//         }
-//     })
-// }else{
-//     fs.appendFile("./new.js","\n function test2() { console.log() } \n module.exports = test;",(err)=>{
-//         console.log("done")
-//     })
-// }
-
-
-
-// fs.readFile("./new.txt",{encoding:"utf-8"},(err,data)=>{
-//     if(err){
-//         console.log("failed to read")
-//     } else{
-//         console.log("File read successfully")
-//         console.log("data:", data)
-
-//         fs.writeFile("./second.txt", data, (err)=>{
-//             if(err){
-//                 console.log("failed to write to file")
-//             } else{
-//                 console.log("done")
-//             }
-//         })
-//     } 
-// })
-
-// const importFunction = require("./import")
-
-// const objRecieved = importFunction.obj;
-
-// console.log("node at work")
-
-
-
-
-
-
-
-
-
-
-
-
-// const myFunction = importFunction.mYfunction;
-
-//const fs = require("fs")
-
-// fs.writeFileSync("./data.txt","sdfghjkljhgfd")
-
-// importFunction.dfghjkl
-// importFunction.exportVar;
-// console.log(importFunction)
-// importFunction.mYfunction()
-// myFunction()
-
-// const obj = {
-//     name: "sdfghjkl;'",
-//     title: "fdghjkl;",
-//     page: "4567890"
-// }
+//     if((req.url==="/home" || req.url==="/" || req.url==="/index") && req.method=="get"){
+//         return res.end("welcome to the home page")
+//     }else if(true){
+//         return res.end("404 no resource found for this address")
+//     }else if(true){
+//         return res.end("404 no resource found for this address")
+//     }else if(true){
+//         return res.end("404 no resource found for this address")
+//     }else if(true){
+//         return res.end("404 no resource found for this address")
+//     }else if(true){
+//         return res.end("404 no resource found for this address")
+//     }else if(true){
+//         return res.end("404 no resource found for this address")
+//     }else if(true){
+//         return res.end("404 no resource found for this address")
+//     }else if(true){
+//         return res.end("404 no resource found for this address")
+//     }else if(true){
+//         return res.end("404 no resource found for this address")
+//     }v
+    
+// }).listen(5000)
